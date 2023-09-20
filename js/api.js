8093caeb7f2dc648caa8aeb81326ffb2506f876f
Elnur <132407067+HQRaffaello@users.noreply.github.com>
2023-09-20 12:58:48 +0300

    2.17 v1.0

---
+++ js/api.js
@@ -0,0 +1,82 @@
+const commentsUrl = 'https://wedev-api.sky.pro/api/v2/elnur-kakhramanov/comments'
+const userUrl = 'https://wedev-api.sky.pro/api/user/login'
+const userRegistr = 'https://wedev-api.sky.pro/api/user'
+
+export let token
+
+export const setToken = (newToken) => {
+	token = newToken
+}
+
+export function getComments() {
+	return fetch(commentsUrl, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}).then((response) => {
+		if (response.status === 401) {
+			throw new Error('Нет авторизации')
+		}
+		return response.json()
+	})
+}
+
+export function postComment({ text, name }) {
+	return fetch(commentsUrl, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify({
+			text: text
+				.replaceAll('&', '&amp;')
+				.replaceAll('<', '&lt;')
+				.replaceAll('>', '&gt;')
+				.replaceAll('"', '&quot;')
+				.replaceAll('QUOTE_BEGIN', "<div class='quote'>")
+				.replaceAll('QUOTE_END', '</div>'),
+			name: name
+				.replaceAll('&', '&amp;')
+				.replaceAll('<', '&lt;')
+				.replaceAll('>', '&gt;')
+				.replaceAll('"', '&quot;')
+				.replaceAll('QUOTE_BEGIN', "<div class='quote'>")
+				.replaceAll('QUOTE_END', '</div>'),
+			// forceError: true,
+		}),
+	}).then((response) => {
+		if (response.status === 500) {
+			throw new Error('Ошибка сервера')
+		} else if (response.status === 400) {
+			throw new Error('Неверный запрос!')
+		} else {
+			return response.json()
+		}
+	})
+}
+
+export function login({ login, password }) {
+	return fetch(userUrl, {
+		method: 'POST',
+		body: JSON.stringify({
+			login,
+			password,
+		}),
+	}).then((response) => {
+		return response.json()
+	})
+}
+
+export function register({ login, password, name }) {
+	return fetch(userRegistr, {
+		method: 'POST',
+		body: JSON.stringify({
+			login,
+			password,
+			name,
+		}),
+	}).then((response) => {
+		return response.json()
+	})
+}
